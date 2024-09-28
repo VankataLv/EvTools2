fd23f49899c7665faf1eca863ba68015508febd3
VankataLv <132002763+VankataLv@users.noreply.github.com>
2024-09-28 16:18:46 +0300

    29.09.2024

--- EvTools2/common/urls.py
+++ EvTools2/common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from EvTools2.common.views import index
+from EvTools2.common.views import index, under_construction
 
 urlpatterns = (
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('under_construction/', under_construction, name='under construction'),
 )
--- EvTools2/common/views.py
+++ EvTools2/common/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'common/index.html', context)
+
+
+def under_construction(request):
+    context = {}
+    return render(request, 'common/under-construction.html', context)
